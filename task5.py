d5442a870f9b739a6cc816b6134ed37d3d122cfa
Areeba Shakir <areeba@gmail.com>
2019-12-19 10:34:20 +0500

    Cleaned the code by removing commented code

--- task5.py
+++ task5.py
@@ -24,34 +24,13 @@ def calc():
     elif op_char == '/':
         result = operand1 / operand2
 
-    # mycollection.drop()
-    # collections_1.drop()
+    
     mydict = {"operand1": operand1, "operand2": operand2, "op_char": op_char, "result": result}
-    #y = collections_1.insert_one(mydict)
-    #print(y)
-    #y1 = dumps(collections_1.find({}, {'_id': False}))
-    #total_records = collections_1.count()
 
-    operator_dict = {'+': {"operand1": "", "operand2": "", "op_char": ""},
-                     '-': {"operand1": "", "operand2": "", "op_char": ""},
-                     '*': {"operand1": "", "operand2": "", "op_char": ""},
-                     '/': {"operand1": "", "operand2": "", "op_char": ""}
-                     }
-
-    # collections_2.insert(operator_dict)
-    # for i in operator_dict:
-    #     if(i==op_char):
-    # myquery = {op_char: "op_char"}
-    # newvalues = {"$set": {"operand1": "operand1", "operand2":"operand2"}}
-    # collections_2.update_one(myquery, newvalues)
-
-    #collections_2.drop()
-    # mycollection.insert_one(mydict)
 
     if  not mycollection.find({},{"op_char":op_char}):
-            #{"op_char":op_char})==0:
-            #find({},{"op_char":op_char}):
-            mycollection.insert_one(mydict)
+
+        mycollection.insert_one(mydict)
     else:
         myquery = {"op_char": op_char}
         newvalues = {"$set": {"operand1": operand1, "operand2": operand2, "result":result}}
@@ -63,12 +42,8 @@ def calc():
         mylist.append(records)
     print(mylist)
     total_records = mycollection.count()
-    #collections_2.drop()
-    # for x in collections_2.find():
-    #     print(x)
-    # li = collections_1.getLastInsertedDocument
-    # print(li)
-    return jsonify('result', result, 'records',mylist,'total_records', total_records)#, "total_records", total_records)
+
+    return jsonify('result', result, 'records',mylist,'total_records', total_records)#"total_records", total_records)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)  # run app in debug mode on port 5000
